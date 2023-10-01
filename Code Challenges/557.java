7903c66e2da1ff8aeb417ff46143c31b4cbfe2bc
shashank651156 <64001714+shashank651156@users.noreply.github.com>
2023-10-01 21:56:19 +0530

    create 557.java

---
+++ Code Challenges/557.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String reverseWords(String s) {
+        // Split the input string into words using space as the delimiter
+        String[] words = s.split(" ");
+        StringBuilder reversedWords = new StringBuilder();
+        
+        // Iterate through each word and reverse it, then append it to the result
+        for (String word : words) {
+            reversedWords.append(new StringBuilder(word).reverse()).append(" ");
+        }
+        
+        // Convert the StringBuilder to a string, and trim any leading/trailing spaces
+        return reversedWords.toString().trim();
+    }
+}
