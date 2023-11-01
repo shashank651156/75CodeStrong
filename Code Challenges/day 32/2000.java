b4767ee6d20fbdb03e8fad3febdfa33b05e33c38
shashank651156 <64001714+shashank651156@users.noreply.github.com>
2023-11-02 00:11:58 +0530

    create day 32

---
+++ Code Challenges/day 32/2000.java
@@ -0,0 +1,8 @@
+class Solution{
+    public String reversePrefix(String word, char ch){
+        int idx = word.indexOf(ch);
+        if(idx==-1)return word;
+        StringBuilder sb  = new StringBuilder(word.substring(0, idx+1));
+        return sb.reverse().toString()+word.substring(idx+1, word.length());
+    }
+}
